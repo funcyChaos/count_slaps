eb016da0fa5f94ba921c75eaf073b6a509d246cf
Austin Reilly <areilly@alphaworks.tech>
2022-07-01 14:47:45 -0700

    add bonus add kill cause it expired need updates

--- count_slaps.php
+++ count_slaps.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 /*
 	Plugin Name: count_slaps
@@ -21,27 +21,47 @@ function count_slaps(){
 		update_option('slap1', ++$result['slap1']);
 	}else{
 		
-		$result['slap2'] = get_option('slap2', 0);
-		update_option('slap2', ++$result['slap2']);
+		if($_REQUEST['bonus'] == 'true'){
+
+			$result['slap2'] = get_option('slap2', 0);	
+			update_option('slap2', $result['slap2'] + 6);
+		}else{
+
+			$result['slap2'] = get_option('slap2', 0);
+			update_option('slap2', ++$result['slap2']);
+		}
 	}
 	
-	echo json_encode($result);
+	$itsOver['final'] = 'fuck off its over';
+	
+	// echo json_encode($result);
+	echo json_encode($itsOver);
 	
 	die();
 }
 
 // This guy is hooking count_slaps into the admin-ajax file
-add_action("wp_ajax_count_slaps", "count_slaps");
-
-// function my_must_login(){
-	
-	// 	$result['type'] = "must login";
-	// 	$result = json_encode($result);
-	// 	echo $result;
-	// 	die();
-	// }
-	
+add_action("wp_ajax_count_slaps", "count_slaps");	
 add_action("wp_ajax_nopriv_count_slaps", "count_slaps");
+
+function return_slaps(){
+	
+	if(!wp_verify_nonce($_REQUEST['nonce'], "count_slaps_nonce")){
+
+		exit("No naughty business please");
+	}
+	
+	$return['slap1'] = get_option('slap1', 0);
+	$return['slap2'] = get_option('slap2', 0);
+	
+	echo json_encode($return);
+	
+	die();
+}
+
+// This guy is hooking count_slaps into the admin-ajax file
+add_action("wp_ajax_return_slaps", "return_slaps");	
+add_action("wp_ajax_nopriv_return_slaps", "return_slaps");
 
 function my_script_enqueuer(){
 	
@@ -64,32 +84,16 @@ function count_slaps_styles(){
 add_action('wp_enqueue_scripts', 'count_slaps_styles');
 
 function slap_btn_1(){
-
-	$nonce = wp_create_nonce("count_slaps_nonce");
+	
 	?>
-
-	<button
-		onclick="count_slaps('slap1')"
-		id="nonce-btn"
-		data-nonce="<?php echo $nonce;?>"
-		class="slap-button"
-	>
+	<button onclick="count_slaps('slap1')" class="slap-button">
 		SLAP!
 	</button>
 	<?php
 }
 
-function slap_cnt_1(){
-	
-	?>
-	<h4 id="slap1">
-		<?php echo get_option('slap1', 0);?>
-	</h4>
-	<?php
-}
-
 function slap_btn_2(){
-
+	
 	?>
 	<button onclick="count_slaps('slap2')" class="slap-button">
 		SLAP!
@@ -97,17 +101,17 @@ function slap_btn_2(){
 	<?php
 }
 
-function slap_cnt_2(){
+function nonce_div(){
 
+	$nonce = wp_create_nonce("count_slaps_nonce");
 	?>
-	<h4 id="slap2">
-	<?php echo get_option('slap2', 0);?>
-	</h4>
+		<div
+			id="nonce-div"
+			data-nonce="<?php echo $nonce;?>"
+		></div>
 	<?php
 }
 
-
 add_shortcode('slap_btn_1' , 'slap_btn_1');
 add_shortcode('slap_btn_2' , 'slap_btn_2');
-add_shortcode('slap_cnt_1', 'slap_cnt_1');
-add_shortcode('slap_cnt_2', 'slap_cnt_2');
+add_shortcode('nonce_div', 'nonce_div');
--- count_slaps_aj.js
+++ count_slaps_aj.js
@@ -1,8 +1,37 @@
+let bonusBool = false;
+
+document.addEventListener('DOMContentLoaded', event=>{
+
+	const counter1 = document.getElementById('slap1');
+	const counter2 = document.getElementById('slap2');
+	const nonce = document.getElementById('nonce-div').getAttribute('data-nonce');
+	
+	bonusBool = (parseInt(counter2.innerHTML) + 1) % 666 ? true : false;
+	
+	fetch(myAjax.ajaxurl, {
+	
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+		},
+		body: `action=return_slaps&nonce=${nonce}`
+	})
+	.then(res=>res.json())
+	.then(object=>{
+		
+		counter1.innerHTML = object['slap1'];
+		counter2.innerHTML = object['slap2'];
+	
+		console.log(object);
+	});
+});
+
+
 function count_slaps(vote){
 
 	const counter1 = document.getElementById('slap1');
 	const counter2 = document.getElementById('slap2');
-	const nonce = document.getElementById('nonce-btn').getAttribute('data-nonce');
+	const nonce = document.getElementById('nonce-div').getAttribute('data-nonce');
 
 	fetch(myAjax.ajaxurl, {
 
@@ -10,7 +39,7 @@ function count_slaps(vote){
 		headers: {
 			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 		},
-		body: `action=count_slaps&nonce=${nonce}&slap=${vote}`
+		body: `action=count_slaps&nonce=${nonce}&slap=${vote}&bonus=${bonusBool}`
 	})
 	.then(res=>res.json())
 	.then(object=>{
@@ -21,8 +50,26 @@ function count_slaps(vote){
 		}else{
 
 			counter2.innerHTML = object['slap2'];
+			
+			if(bonusBool){
+
+				showBonus('team2');
+				console.log(bonusBool);
+			}
 		}
 
 		console.log(object);
 	});
+}
+
+function showBonus(team){
+
+	bonus = document.getElementById(`${team}-bonus`);
+
+	bonus.style.opacity = 1;
+
+	setTimeout(()=>{
+		
+		bonus.style.opacity = 0;
+	}, 100);
 }
